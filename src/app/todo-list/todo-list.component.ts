c6239b16d656a534c84acc163d9690e70488fca0
Iryna <gresrin@gmail.com>
2023-03-29 22:56:32 +0200

    Add list

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  tasks = [
+    {name: 'Task 1', completed: false},
+    {name: 'Task 2', completed: false},
+    {name: 'Task 3', completed: false},
+  ]
 
   constructor() { }
 
